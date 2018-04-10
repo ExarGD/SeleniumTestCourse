3b5aa2235ebb5fa870276c2cedf13f1002d71ccf
Адель <exar@Adel-MacBook-Pro.local>
2018-04-10 20:01:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+href = "https://www.google.com"
+
+class testCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.wd = webdriver.Chrome()
+
+
+    def test_1(self):
+        self.wd.get(href)
+        self.wd.find_element_by_name("q").send_keys("Selenium was here")
+        self.wd.find_element_by_name("btnK").click()
+        WebDriverWait(self.wd, 10).until(EC.title_is("Selenium was here - Google Search"))
+
+
+    def tearDownClass(self):
+        self.wd.quit()
+
+if __name__ == '__main__':
+    unittest.main()
