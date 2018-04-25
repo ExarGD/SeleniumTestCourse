4957fb40ace6dc98a06585b6175489d67557b0ac
Адель <exar@Adel-MacBook-Pro.local>
2018-04-25 23:45:10 +0300

    Task 8

--- main.py
+++ main.py
@@ -1,28 +1,11 @@
-import unittest
+import time
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-href = "https://www.google.com"
-
-class testCase(unittest.TestCase):
-    @classmethod
-    def setUpClass(self):
-        self.wd = webdriver.Chrome()
-
-
-    def test_1(self):
-        self.wd.get(href)
-        self.wd.find_element_by_name("q").send_keys("Selenium was here")
-        self.wd.find_element_by_name("btnI").click()
-        try:
-            WebDriverWait(self.wd, 10).until(EC.title_is("Selenium was here - Google Search"))
-        except:
-            print("Title mismatch")
-            self.wd.quit()
-
-    def tearDownClass(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+driver = webdriver.Chrome()  # Optional argument, if not specified will search path.
+driver.get('http://www.google.com/xhtml');
+time.sleep(5) # Let the user actually see something!
+search_box = driver.find_element_by_name('q')
+search_box.send_keys('ChromeDriver')
+search_box.submit()
+time.sleep(5) # Let the user actually see something!
+driver.quit()
--- task1.py
+++ task1.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-href = "http://192.168.64.2/litecart/admin/"
+href = "http://localhost/litecart/admin/"
 
 
 class testCase(unittest.TestCase):
